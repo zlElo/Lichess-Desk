ec516ba4b7d13570a8f1bda4c936f445d5a2f03a
zlElo <108819252+zlElo@users.noreply.github.com>
2022-10-12 08:46:00 +0200

    Update ping.sh

--- Lichess-Desktop/ping.sh
+++ Lichess-Desktop/ping.sh
@@ -3,7 +3,7 @@ ping -q -c5 lichess.org > /dev/null
 if [ $? -eq 0 ]
 then
     echo "[✓] The Python script called the .sh script"
-	echo "[✓] Connection to internet"
+    echo "[✓] Connection to internet"
     echo "[✓] Lichess Server is online"
     echo "[✓] Connection to lichess server"
     echo "The program will starts now:"
